a8c28323ef28ebbcebff3a7df424a14e1695d808
jydxh <wzxxds@gmail.com>
2024-05-26 13:24:20 -0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/redo_comfy_store/",
+	base: "/comfy_store_2/",
 	plugins: [react()],
 	resolve: {
 		alias: {

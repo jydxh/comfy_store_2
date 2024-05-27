ac81b6d2e2b915c36ce971d811ff5da69533ce90
jydxh <wzxxds@gmail.com>
2024-05-27 12:44:06 -0400

    ux

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,13 @@
 function About() {
 	return (
 		<main className="dark:text-white p-10 mt-16 mx-auto flex flex-col justify-center text-center max-w-[1280px]">
-			<h2 className="font-bold text-6xl ">
+			<h2 className="font-bold text-4xl md:text-6xl ">
 				We Love
-				<span className="tracking-wider bg-blue-600 rounded-xl px-4 mx-8">
+				<span className="tracking-wider bg-blue-600 rounded-xl px-4 mx-4 md:mx-8 my-0">
 					Comfy
 				</span>
 			</h2>
-			<article className="mt-10 text-xl w-[80%] mx-auto text-start ">
+			<article className="mt-10 text-xl md:text-2xl w-[80%] mx-auto text-start ">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis porro
 				provident iure. Ut recusandae, sit maxime veniam aut accusamus omnis
 				corrupti alias, unde officia minima aliquid voluptas eius tempore dolor,
